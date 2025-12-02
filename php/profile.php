b360985d01af052544761ed17f2f37454dda1003
Rand <randshalabi@hotmail.com>
2025-12-02 19:37:26 +0200

    add static donor profile

---
+++ php/profile.php
@@ -0,0 +1,40 @@
+<?php 
+    $title = "BDMS - Donor Profile";
+    include "includes/header.php";
+?>
+<div class="profile-container">
+    <div class="profile-card text-center p-4">
+        <div class="row">
+            <div class="col-12">
+                <img class="profile" src="../assets/blank_profile.webp" alt="">
+            </div>
+            <div class="col-12">
+                <h2 class="profile-title">Donor Name</h2>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-12 profile-info">
+                <h5>Contact Information</h5>
+                <small class="text-muted">Email</small>
+                <div>donor1234@example.com</div>
+                <small class="text-muted">Phone</small>
+                <div>+972 598 331 743</div>
+            </div>
+            <div class="col-12 profile-info">
+                <h5>Medical Information</h5>
+                <small class="text-muted">Blood Type</small>
+                <div>O+</div>
+                <small class="text-muted">Gender</small>
+                <div>male</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 profile-info">
+                <h5>Account Information</h5>
+                <small class="text-muted">Member Since</small>
+                <div>2022-11-03</div>
+            </div>
+        </div>
+    </div>
+</div>
