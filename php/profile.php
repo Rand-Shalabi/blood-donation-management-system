6080f1aeee4b7b6e0f62038a3b8334c68b278bbe
Rand <randshalabi@hotmail.com>
2025-12-02 20:08:25 +0200

    add missing tags and correct indentation in profile.php

--- php/profile.php
+++ php/profile.php
@@ -2,39 +2,44 @@
     $title = "BDMS - Donor Profile";
     include "includes/header.php";
 ?>
-<div class="profile-container">
-    <div class="profile-card text-center p-4">
-        <div class="row">
-            <div class="col-12">
-                <img class="profile" src="../assets/blank_profile.webp" alt="">
+<body>
+    <div class="profile-container">
+        <div class="profile-card text-center p-4">
+            <div class="row">
+                <div class="col-12">
+                    <img class="profile" src="../assets/blank_profile.webp" alt="">
+                </div>
+                <div class="col-12">
+                    <h2 class="profile-title">Donor Name</h2>
+                </div>
             </div>
-            <div class="col-12">
-                <h2 class="profile-title">Donor Name</h2>
+            <hr>
+            <div class="row">
+                <div class="col-12 profile-info">
+                    <h5>Contact Information</h5>
+                    <small class="text-muted">Email</small>
+                    <div>donor1234@example.com</div>
+                    <small class="text-muted">Phone</small>
+                    <div>+972 598 331 743</div>
+                </div>
             </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-12 profile-info">
-                <h5>Contact Information</h5>
-                <small class="text-muted">Email</small>
-                <div>donor1234@example.com</div>
-                <small class="text-muted">Phone</small>
-                <div>+972 598 331 743</div>
+            <div class="row">
+                <div class="col-12 profile-info">
+                    <h5>Medical Information</h5>
+                    <small class="text-muted">Blood Type</small>
+                    <div>O+</div>
+                    <small class="text-muted">Gender</small>
+                    <div>male</div>
+                </div>
             </div>
-            <div class="col-12 profile-info">
-                <h5>Medical Information</h5>
-                <small class="text-muted">Blood Type</small>
-                <div>O+</div>
-                <small class="text-muted">Gender</small>
-                <div>male</div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12 profile-info">
-                <h5>Account Information</h5>
-                <small class="text-muted">Member Since</small>
-                <div>2022-11-03</div>
+            <div class="row">
+                <div class="col-12 profile-info">
+                    <h5>Account Information</h5>
+                    <small class="text-muted">Member Since</small>
+                    <div>2022-11-03</div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</body>
+</html>
