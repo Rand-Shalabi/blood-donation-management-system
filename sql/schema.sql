7b5213b06cf7afef818a6e3cb37f32d065970f03
Rand <randshalabi@hotmail.com>
2025-12-04 15:46:35 +0200

    seed admin into bdms db

--- sql/schema.sql
+++ sql/schema.sql
@@ -23,3 +23,7 @@ CREATE TABLE donor (
     donations INT DEFAULT 0,
     FOREIGN KEY (donor_id) REFERENCES user(id)
 );
+
+-- Seed admin account with hashed password
+INSERT INTO user (created_at, email, password, role)
+VALUES(CURDATE(), 'admin@example.com', '$2y$10$aJ3ltFpL0cig3b0PPWzLduIi1zCuXADQz8FjWAGzoRYsr3zKdfk/K', 'admin');
