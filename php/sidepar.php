987fcfa35a25445df0c6438cb2589cc3f0f35407
shahdwj <shoshjallad@gmil.com>
2025-12-07 16:17:27 +0200

    sidbar section

---
+++ php/sidepar.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <ul>
+        <li><a href="add-donor.php">Add Donor</a></li>
+        <li><a href="donor-list.php">Donor List</a></li>
+        <li><a href="delete-donor.php">Delete Donor</a></li>
+        <li><a href="edit-donor.php">Update Donor Info</a></li>
+        <li><a href="change-pass.php">Change Password</a></li>
+    </ul>
+</div>
