777c7c216e6967ae160089c023c10ab939ff9692
Rand <randshalabi@hotmail.com>
2025-12-08 17:27:36 +0200

    add php logic to query donor info and render in profile page
    replaced hardcoded profile values with dynamic variables fetched from the database using donor email.

--- php/profile.php
+++ php/profile.php
@@ -1,16 +1,39 @@
 <?php 
     $title = "BDMS - Donor Profile";
     include "includes/header.php";
+
+    include "includes/connection.php";
+    $email = $_POST["email"];
+    $sql = "SELECT donor.*, user.created_at
+            FROM donor join user
+            ON donor.donor_id = user.id 
+            WHERE email = '$email'";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($row = $result->fetch_assoc()){
+        $name = $row['full_name'];
+        $photo = $row['photo'];
+        $phone = $row['phone'];
+        $blood_type = $row['blood_type'];
+        $last_donation = $row['last_donation'];
+        $donations = $row['donations'];
+        $gender = $row['gender'];
+        $created_at = $row['created_at'];
+    }
+    $formatted_phone = substr($phone, 0, 3) . "-" . substr($phone, 3, 3) . "-" . substr($phone, 6);
 ?>
 <body>
     <div class="profile-container">
         <div class="profile-card text-center p-4">
             <div class="row">
                 <div class="col-12">
-                    <img class="profile" src="../assets/blank_profile.webp" alt="">
+                    <img class="profile" src= "<?= "../uploads/" . $photo ?>" alt="">
                 </div>
                 <div class="col-12">
-                    <h2 class="profile-title">Donor Name</h2>
+                    <h2 class="profile-title"><?= $name?></h2>
                 </div>
             </div>
             <hr>
@@ -18,29 +41,29 @@
                 <div class="col-12 profile-info">
                     <h5>Contact Information</h5>
                     <small class="text-muted">Email</small>
-                    <div>donor1234@example.com</div>
+                    <div><?= $email?></div>
                     <small class="text-muted">Phone</small>
-                    <div>+972 598 331 743</div>
+                    <div><?= "+972 " . $formatted_phone?></div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 profile-info">
                     <h5>Medical Information</h5>
                     <small class="text-muted">Blood Type</small>
-                    <div>O+</div>
+                    <div><?= $blood_type?></div>
                     <small class="text-muted">Last Donation</small>
-                    <div>2025-04-22</div>
+                    <div><?= $last_donation?></div>
                     <small class="text-muted">Donations</small>
-                    <div>4</div>
+                    <div><?= $donations?></div>
                     <small class="text-muted">Gender</small>
-                    <div>male</div>
+                    <div><?= $gender?></div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-12 profile-info">
                     <h5>Account Information</h5>
                     <small class="text-muted">Member Since</small>
-                    <div>2022-11-03</div>
+                    <div><?= $created_at?></div>
                 </div>
             </div>
         </div>
