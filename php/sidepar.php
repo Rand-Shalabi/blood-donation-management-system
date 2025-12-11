3b9d1b131999f8d5f606af7ce03451dbf647c152
Rand <randshalabi@hotmail.com>
2025-12-11 21:27:28 +0200

    update sidebar list

--- php/sidepar.php
+++ php/sidepar.php
@@ -2,8 +2,8 @@
     <ul>
         <li><a href="add-donor.php">Add Donor</a></li>
         <li><a href="donor-list.php">Donor List</a></li>
+        <li><a href="edit-donor.php">Update Donor</a></li>
+        <li><a href="change-pass.php">Reset Password</a></li>
         <li><a href="delete-donor.php">Delete Donor</a></li>
-        <li><a href="edit-donor.php">Update Donor Info</a></li>
-        <li><a href="change-pass.php">Change Password</a></li>
     </ul>
 </div>
