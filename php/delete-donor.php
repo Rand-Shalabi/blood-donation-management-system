0189594f63c14ce6e73c6463e7c8f08bbba555ea
shahdwj <shoshjallad@gmil.com>
2025-12-15 15:41:34 +0200

    delete donor logic with validation and messages

--- php/delete-donor.php
+++ php/delete-donor.php
@@ -2,18 +2,24 @@
 $title = "BDMS - Delete Donor";
 include "includes/header.php";
 include "includes/connection.php";
-
+include "functions.php";
+ if(isset($_GET['donor_id'])){
+        $donor = (int)$_GET['donor_id'];
+ }
  if (isset($_POST['delete'])) {
-    $donor_id = $_POST['donor_id'];
+    $donor_id = (int)$_POST['donor_id'];
 
-    $query = "DELETE FROM donor WHERE donor_id = '$donor_id'";
-    $result = mysqli_query($conn, $query);
-
-    if ($result) {
-        echo "<div class='alert alert-success'>Donor deleted successfully</div>";
-    } else {
-        echo "<div class='alert alert-danger'>Error deleting donor</div>";
+    $check = "SELECT donor_id FROM donor WHERE donor_id = $donor_id";
+    $check_result = mysqli_query($conn, $check);
+    if(mysqli_num_rows($check_result) == 0){
+       $error = "Donor ID not found.";
+    } else{
+        $query = "DELETE FROM donor WHERE donor_id = $donor_id";
+        $result = mysqli_query($conn, $query);
+        $success ="Donor Deleted Successfully!";
+        
     }
+
 }
 ?>
 <body>
@@ -24,10 +30,17 @@ include "includes/connection.php";
     <div class="content">
      <div class="container-delete ">
     <h2>Delete Donor</h2>
-
-     <form method="POST" class="delete-box">
+        <?php 
+            if(isset($error)){
+                echo show_alert($error);
+            } elseif(isset($success)){
+                header("refresh:2;url=donor-list.php");
+                echo show_alert($success, "success");
+            }
+        ?>
+     <form method="POST" class="delete-box" enctype="multipart/form-data">
         <label>Enter Donor ID:</label>
-        <input type="number" name="donor_id" required>
+        <input type="number" name="donor_id" value="<?= isset($donor)? $donor: ""; ?>" required>
  
         <input type="submit" name="delete" value="Delete Donor" class=" btn btn-delete btn-danger ">
     </form>
